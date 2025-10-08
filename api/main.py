8d6d471cae3d14519f7130e19993be06963528eb
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-10-08 12:55:07 +0200

    add connection to api

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 app = FastAPI()
+
+#engine = create_engine("postgresql://localhost:5435/aware_me_db")
 
 @app.get("/")
 def welcome(name: str = "Harold"):
     return {"message": f"Hello {name}"}
+
+@app.post("/appusagedata")
+def app_usage_data(name: str = "Test"):
+    return {"data": f"Connction etablished with {name}"}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,83 +1,133 @@
+/*
 import 'package:flutter/material.dart';
+import 'screens/usage_stats_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppUsageTrackerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class AppUsageTrackerApp extends StatelessWidget {
+  const AppUsageTrackerApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App Usage Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const UsageStatsScreen(),
     );
   }
 }
+*/
+import 'dart:convert';
+import 'dart:math';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+import 'package:flutter/material.dart';
+import 'package:app_usage/app_usage.dart';
+import 'package:installed_apps/app_info.dart';
+import 'package:logger/logger.dart';
+import 'package:installed_apps/installed_apps.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(AppUsageApp());
+
+class AppUsageApp extends StatefulWidget {
+  const AppUsageApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  AppUsageAppState createState() => AppUsageAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AppUsageAppState extends State<AppUsageApp> {
+  List<AppUsageInfo> _infos = [];
+  var logger = Logger();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      print("Plus clicked");
-    });
+  @override
+  void initState() {
+    super.initState();
   }
 
-  void _decrement() {
-    setState(() {
-      _counter--;
-    });
+  void getUsageStats() async {
+    try {
+      DateTime now = DateTime.now();
+      DateTime startOfDay = DateTime(now.year, now.month, now.day, 5, 1);
+      List<AppUsageInfo> infoList = await AppUsage().getAppUsage(
+        startOfDay,
+        now,
+      );
+      List<AppUsageInfo> normalized = infoList.map((info) {
+        return AppUsageInfo(
+          info.packageName,
+          info.usage.inSeconds.toDouble(),
+          startOfDay,
+          info.endDate,
+          info.lastForeground,
+        );
+      }).toList();
+      logger.i(normalized);
+      setState(() => _infos = normalized);
+    } catch (exception) {
+      logger.e(exception);
+    }
+  }
+
+  Future<void> sendToAPI() async {
+    var url = "https://b9304ae91e3f.ngrok-free.app/appusagedata?name=Flutter";
+
+    try {
+      var repsonse = await http.post(Uri.parse(url));
+      logger.i(jsonDecode(repsonse.body));
+    } catch (exception) {
+      logger.e(exception);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'App Usage Data',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+        body: ListView.builder(
+          itemCount: _infos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_infos[index].appName),
+
+              trailing: Text(_infos[index].usage.toString()),
+            );
+          },
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          spacing: 15,
+          children: [
+            FloatingActionButton(
+              onPressed: getUsageStats,
+              child: Icon(Icons.file_download),
+            ),
+            FloatingActionButton(
+              onPressed: () => sendToAPI(),
+              child: Icon(Icons.send),
             ),
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        spacing: 10,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: _decrement,
-            tooltip: "Decrement",
-            child: const Icon(Icons.remove),
-          ),
-        ],
       ),
     );
   }
