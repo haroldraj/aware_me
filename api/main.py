fd7a35c914572dd5a7ab32a74af3e282b1dc1496
Harold <rnarsonharold@gmail.com>
2025-10-10 23:22:34 +0200

    add sample for the event logs

--- api/main.py
+++ api/main.py
@@ -12,6 +12,11 @@ class AppUsageData(BaseModel):
     usage: int
     startDate: datetime
     endDate: datetime
+    lastForegroundDate: datetime
+
+
+class EventData(BaseModel):
+    name: str
 
 
 class AppUsage(SQLModel, table=True):
@@ -22,14 +27,16 @@ class AppUsage(SQLModel, table=True):
     usage: int
     startDate: datetime
     endDate: datetime
-
+    lastForegroundDate: datetime
 
 
 app = FastAPI()
 
-engine = create_engine("postgresql://aware_me_user:aware_me_password@localhost:5435/aware_me_db")
+engine = create_engine(
+    "postgresql://aware_me_user:aware_me_password@localhost:5435/aware_me_db")
 
 SQLModel.metadata.create_all(engine)
+
 
 @app.get("/")
 def welcome(name: str = "Harold"):
@@ -51,9 +58,15 @@ def app_usage_data(appusagedata: list[AppUsageData]):
                 appName=data.appName,
                 usage=data.usage,
                 startDate=data.startDate,
-                endDate=data.endDate
+                endDate=data.endDate,
+                lastForegroundDate=data.lastForegroundDate
             )
             session.add(record)
         session.commit()
 
     return {"message": f"{len(appusagedata)} records saved successfully."}
+
+
+@app.post("/event_data")
+def event_data(eventdata: list[EventData]):
+    return eventdata
